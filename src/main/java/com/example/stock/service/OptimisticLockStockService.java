9cb2760973ef41befd552877aa12ce51f5b0a267
cyh <drsggg>
2023-07-06 17:19:42 +0900

    FEAT : OptimisticLockStockService 코드 작성

---
+++ src/main/java/com/example/stock/service/OptimisticLockStockService.java
@@ -0,0 +1,25 @@
+package com.example.stock.service;
+
+import com.example.stock.domain.Stock;
+import com.example.stock.repository.StockRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class OptimisticLockStockService {
+
+    private final StockRepository stockRepository;
+
+    /**
+     * 재고 감소
+     */
+    @Transactional(propagation = Propagation.REQUIRED)
+    public void decrease(final Long id, final Long quantity) {
+        Stock stock = stockRepository.findByIdWithOptimisticLock(id);
+        stock.decrease(quantity);
+//        stockRepository.saveAndFlush(stock);
+    }
+}
