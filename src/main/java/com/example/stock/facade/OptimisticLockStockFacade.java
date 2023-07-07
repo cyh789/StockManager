549b48bcc98fdbca18386c9785a91f8c917061b2
cyh <drsggg>
2023-07-07 15:32:00 +0900

    REFACTOR : OptimisticLockStockService 코드 변수명 수정

--- src/main/java/com/example/stock/facade/OptimisticLockStockFacade.java
+++ src/main/java/com/example/stock/facade/OptimisticLockStockFacade.java
@@ -8,12 +8,12 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class OptimisticLockStockFacade {
 
-    private final OptimisticLockStockService stockService;
+    private final OptimisticLockStockService optimisticLockStockService;
 
     public void decrease(final Long id, final Long quantity) throws InterruptedException {
         while (true) {
             try {
-                stockService.decrease(id, quantity);
+                optimisticLockStockService.decrease(id, quantity);
                 break;
             } catch (Exception e) {
                 Thread.sleep(50);
